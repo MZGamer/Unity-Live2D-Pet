fd3d763f90b92c010ee77dd6f8c8688fbfb180c9
BuildTools <sacred97093@gmail.com>
2020-02-29 18:46:12 +0800

    New
    new

---
+++ Live2D/Assets/Live2D/Cubism/Framework/LookAt/ObFollowMouse.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObFollowMouse : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+	}
+}
